3a1ad086c3e757c5e98e418002c253d417c4b3df
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-04-19 21:11:03 +0530

    Time: 3 ms (64.27%), Space: 8.3 MB (10.85%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    unordered_map<int, int> map;
+
+    int fib(int n) {
+        map[0] = 0;
+        map[1] = 1;
+        return recursion(n);
+    }
+
+    int recursion(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        if (map.count(n)) {
+            return map[n];
+        }
+
+        map[n] =recursion(n - 1) + recursion(n - 2);
+        return map[n];
+    }
+};
+
+/**
+
+n = 4
+
+            fib(4) = 3
+          /              \   
+       2 fib(3)      +          fib(2) -> 1
+        /       \               |
+    1 fib(2)     fib(1) = 1     fib(1) + fib(0)
+     /
+    fib(1) + fib(0) = 0
+       1 
+
+
+--------------------
+
+map: 
+
+[
+
+1: 1
+0: 0
+2: 1
+
+]
+    f(n) = f(n - 1) + f(n - 2)
+
+            f(4) = f(3) + f(2) = 2 + 1 = 3
+            /       \
+      2  f(3)   +     f(2) = 1
+        / \
+  1  f(2) + f(1) = 1 
+    /   \
+    f(1) f(0)
+    1   +  0
+
+
+
+**/
