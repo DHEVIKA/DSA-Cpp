219acc3ab66fec1869fce5007a3b2f290249766f
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-04-21 19:22:48 +0530

    Time: 0 ms (100%), Space: 97.2 MB (86.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;
-        int maxProfit = 0;
+        int lsf = INT_MAX; // least so for (lsf)
+        int op = 0; //overall profit  (op)
+        int pist = 0; //  profit if sold today (pist)
 
-        for (int price : prices) {
-            minPrice = min(minPrice, price);
-            maxProfit = max(maxProfit, price - minPrice);
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < lsf){ // if we found new buy value which is more smaller then previous one
+                lsf = prices[i]; // update our least so far
+            }
+            pist = prices[i] - lsf; // calculating profit if sold today by, Buy - sell
+            if(op < pist){ // if pist is more then our previous overall profit
+                op = pist; // update overall profit
+            }
         }
-        return maxProfit;
+        return op; // return op 
     }
 };
+
+/*
+ Remember one rule :- You can only buy one time & sell one time
+
+So, if buy at 7 & sell at any time in the future, we'll face loss. Because buying price is way higher then selling price available we have
+
+Now, I have seen a dip & I buy at 1 & sell at 5 my overall profit will be 5 - 1 = 4
+
+But what if, I had buy at 1 & sell at 6 my profit will be 6 - 1 = 5. Which is greater then my overall profit. So, i will update my overall profit with new value.
+
+Now we have done as further we don't have any higher point to sell. We will return our answer.
+
+*/
