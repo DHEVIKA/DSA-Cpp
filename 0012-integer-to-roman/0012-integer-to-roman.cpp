515b035b1d05eedf45e0f50a8ea6f445f0296c4f
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-04-23 18:19:28 +0530

    Time: 7 ms (29.74%), Space: 9.1 MB (92.5%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,53 +1,29 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string> r = {
-            "M", "CM", "D", "CD", "C", "XC", "L",
-            "XL", "X", "IX", "V", "IV", "I"
-        };
-
-        vector<int> in = {
-            1000, 900, 500, 400, 100, 90,
-            50, 40, 10, 9, 5, 4, 1
-        };
-
-        int n = r.size();
-        string result;
-        int curNum = num;
-
-        for (int i = 0; i < n && curNum > 0; i++) {
-
-            while (in[i] <= curNum) {
-                curNum -= in[i];
-                result += r[i];
-            }
-
-        }
-
-        return result;
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
 
-/**
+/* The given problem is about converting an integer into a Roman numeral. To do this, we can create arrays that represent the Roman numeral symbols for each place value (ones, tens, hundreds, thousands). Then, we can divide the given number into its respective place values and concatenate the corresponding Roman numeral symbols.
 
-
-
-
-{ "M",  "CM", "D",  "CD", "C",  "XC", "L", "XL",  "X", "IX", "V", "IV", "I" };
-{ 1000, 900,  500,  400,  100,   90,   50,  40,   10,   9,    5,   4,    1 };
-
-
-input: 3749
-
-curNum : 0
-
-ouput: MMMDCCXLIX
-
-
+Approach:
+Create four arrays: ones, tens, hrns, and ths, representing the Roman numeral symbols for ones, tens, hundreds, and thousands respectively. Each array contains the symbols for the numbers from 0 to 9 in their respective place value.
+Divide the given number num into its respective place values:
+// thousands = num / 1000
+// hundreds = (num % 1000) / 100
+// tens = (num % 100) / 10
+// ones = num % 10
+Concatenate the Roman numeral symbols based on the place values:
+ths[num/1000] represents the Roman numeral for thousands place.
+hrns[(num%1000)/100] represents the Roman numeral for hundreds place.
+tens[(num%100)/10] represents the Roman numeral for tens place.
+ones[num%10] represents the Roman numeral for ones place.
+Return the concatenation of the Roman numeral symbols obtained from step 3.
 
 */
