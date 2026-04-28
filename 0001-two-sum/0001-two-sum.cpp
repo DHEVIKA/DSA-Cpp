011d290e7cb8b5c682c5f453b3104704b3a09cfc
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-04-28 19:27:10 +0530

    Time: 0 ms (100%), Space: 14.7 MB (53.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,44 +1,31 @@
 class Solution {
-    
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
-    
-        unordered_map<int,int>map;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            int rem = target-nums[i];
-            if(map.find(rem)!=map.end()){
-                return {map[rem],i};
-            }
-            map[nums[i]]=i;
-        }
-        return {-1,-1};
-
-
         
+        vector<pair<int,int>> arr;
+        for(int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i}); // {value, index}
+        }
+
+        sort(arr.begin(), arr.end());
+
+        int left = 0, right = n - 1;
+
+        while(left < right) {
+            int sum = arr[left].first + arr[right].first;
+
+            if(sum == target) {
+                return {arr[left].second, arr[right].second};
+            }
+            else if(sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return {-1,-1};
     }
 };
-
-
-
-/**
-
-nums = [11,15,2,7], target = 9
-
-u have to do ..
-we r using ##hash map## for fast look up since they didnt tell its in inplace
-
-11-9 = 2 its present in  map no then return it with num an that particular index
-15-9 = 6 not 
-2-9 = -7 not 
-7-9 = -2 not 
-if all the cases doent not satisfy return {-1,-1}
-[
-    11:0 
-    15:1
-    2:2
-
-
-]
-*/
