d70a2ea7f49696197853ba8a5ddbb63acbda0595
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-05-02 22:45:33 +0530

    Time: 3 ms (33.51%), Space: 9.7 MB (17.02%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -6,30 +6,31 @@ public:
 
         for (int i = 0; i <= n; i++) {
             if (i < 10) {
-                if (i == 0 || i == 1 || i == 8) dp[i] = 1;
-                else if (i == 2 || i == 5 || i == 6 || i == 9) {
+                if (i == 0 || i == 1 || i == 8) dp[i] = 1;  // for 0,1,8 considering value as 1
+                else if (i == 2 || i == 5 || i == 6 || i == 9) { //for 2,5,6,9 considering value as 2
                     dp[i] = 2;
                 }
-                else dp[i] = 0;
+                else dp[i] = 0; 
             } else {
-                int last = i % 10;
-                int rem = i / 10;
+                int last = i % 10; // last digit
+                int rem = i / 10; // remaining
 
                 if (dp[rem] == 0) {
                     dp[i] = 0;
                 } 
-                else if (last == 0 || last == 1 || last == 8) {
+                else if (last == 0 || last == 1 || last == 8) { // these 0,1,8 wont change while rotating
                     dp[i] = dp[rem];
                 } 
-                else if (last == 2 || last == 5 || last == 6 || last == 9) {
-                    dp[i] = 2;
+                else if (last == 2 || last == 5 || last == 6 || last == 9) { // 2 as 5 | 5 as 2 | 6 as 9 | 9 as 6
+                    if (dp[rem] == 1) dp[i] = 2;   // becomes good
+                    else if (dp[rem] == 2) dp[i] = 2; // already good
                 } 
                 else {
                     dp[i] = 0;
                 }
             }
 
-            if (dp[i] == 2) count++;   // ✅ count here ONLY
+            if (dp[i] == 2) count++;  
         }
 
         return count;
