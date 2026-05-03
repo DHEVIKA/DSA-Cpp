ce94f430be35077aa54eaa746cefe7293948868f
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-05-03 16:11:43 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.66%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size() != goal.size())
-            return 0;
-        string cat = goal + goal;
-        return cat.find(s) != -1;
+        if (s.length() != goal.length()) return false;
+
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+
+            // store first character
+            char first = s[0];
+
+            
+            for (int j = 0; j < n - 1; j++) { // shift it to left
+                s[j] = s[j + 1];
+            }
+
+            
+            s[n - 1] = first; //1st char at end
+
+           
+            if (s == goal) return true;
+        }
+
+        return false;
     }
 };
-
-// First check if size of s is equal to goal, if no then return false. Concatenate goal twice and check if s is a part of goal and return it.
