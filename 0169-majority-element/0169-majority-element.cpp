280f218f46291feb059fa01dd232e2a6e7e43569
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-05-03 12:48:35 +0530

    Time: 0 ms (100%), Space: 28 MB (98.99%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,22 +2,46 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
 
-    //   for fast look up u can use map 
-    //     [3,2,3]
-    //     [
-    //         3: 1
-    //         2: 1
-    //         3: 1+1 ->already above totally 2 
-    //     ]
-    unordered_map <int,int>map;
-    int n = nums.size() / 2;
-    for(int num:nums){
-        map[num]++;
-    }
-    for(int num:nums){
-        if(map[num]>n)
-            return num;
-    }
-    return -1;
+        int count = 0;
+        int candidate = 0;
+        for(auto num : nums){
+            if(count == 0){
+                candidate = num;
+
+            }
+            if(num == candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+    
+    return candidate;
+
+   
     }
 };
+
+/* Think of this array as a collection of votes from the voters for different political parties.
+Now as we know the party which has >50% votes can form the government.
+
+So , our above question is analogous to this situation.
+Now , if we are certain that one party has > 50% votes . Then , if :
+
+We increment a count variable every time we see the vote from the majority party and decrement it whenever a vote from some other party is occured , we can guarantee that ,
+count>0.
+
+Approach 
+
+Initialize candidate and count.
+
+Traverse array:
+
+If count == 0, set candidate = num.
+If num == candidate, increment count, else decrement count.
+Return candidate as majority element.
+
+Complexity
+Time Complexity: O(n) (single pass)
+Space Complexity: O(1) (no extra space used)  */
