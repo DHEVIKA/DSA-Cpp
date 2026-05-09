a3c61c2ae07082f3840d25bc411e5a7b34185fb1
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-05-09 13:59:52 +0530

    Time: 107 ms (43.29%), Space: 95.4 MB (55.73%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        long long windowSum = 0, maxSum = 0;
+        int l = 0;
+
+        for (int r = 0; r < nums.size(); r++) {
+            windowSum += nums[r];
+            freq[nums[r]]++;
+
+            if (r - l + 1 > k) {
+                windowSum -= nums[l];
+                freq[nums[l]]--;
+                if (freq[nums[l]] == 0) {
+                    freq.erase(nums[l]);
+                }
+                l++;
+            }
+
+            if (r - l + 1 == k && freq.size() == k) {
+                maxSum = max(maxSum, windowSum);
+            }
+        }
+
+        return maxSum;
+    }
+};
+
+/* Here, we need to find a subarray of length k such that:
+-> All elements are distinct
+-> The sum is maximized
+
+A brute-force approach would check every subarray of size k and verify if all elements are distinct, but that would take O(n · k) time, which is too slow.
+
+Instead, we observe that:
+-> We are only dealing with fixed-size windows (k)
+-> So we can use a sliding window to efficiently move from one subarray to the next
+
+As we slide the window:
+-> We add the new element entering the window
+-> We remove the element leaving the window
+-> We maintain a frequency map to track occurrences of elements in the current window
+-> We also maintain the current sum of the window
+
+To check if all elements are distinct:
+If the number of unique elements(length of frequency map) in the map equals k, then all elements are distinct
+Otherwise, duplicates exist
+
+This avoids rechecking the entire window every time and allows us to update everything in constant time per step.
+
+Thus, we efficiently compute the answer in O(n) time using a sliding window and hash map.
+
+*/
