dd31bab46108a641b29e2b6dc464c02a4332dd2c
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-05-10 18:25:00 +0530

    Time: 2 ms (37.15%), Space: 9.7 MB (99.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        /* Intuition
-By reading the question you may think that first we need to convert the string to desired form and then check if its a valid palindrome or not.
-
-But all of that is just distraction you can check valid palindrome by simply using two pointers no need to convert.
-
-Approach:
-
-If a character is not alphanumeric we can simply ignore it and update our pointer to next character (for both pointers)
-and then we check by converting the alphanumeric character to lowercase (tolower() function on numbers has no change)
-if those two are not equal then return false(not palindrome)
-else update both pointers and continue.
-
-Finally after all checking if reach at last then string must be a valid palindrome so return true.
-
-Complexity
-Time complexity:O(n)
-Space complexity:O(1) since only use two pointers */
-       int n = s.size();
-       int left = 0;
-       int right = n-1;
-       while(left <= right){
-           if(!isalnum(s[left]))
-           {
-            left++; 
-            continue;  ///skip it If a character is not alphanumeric
+        int n = s.size();
+        int left = 0;
+        int right = n-1;
+        while(left<=right){
+            if(!isalnum(s[left])){
+                left++;
+                continue;
             }
-           if(!isalnum(s[right]))
-           {
-            right--;
-            continue; ///skip it If a character is not alphanumeric
+            if(!isalnum(s[right])){
+                right--;
+                continue;
             }
-           if(tolower(s[left])!=tolower(s[right])) // then we check by converting the alphanumeric character to lowercase (tolower() function on numbers has no change)
-            return false;
-           else{
-               left++;
-               right--;
-           }
-       }
-       return true;
-}
+            if(tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
 };
+
+/*  “A man, a plan, a canal: Panama” -
+Step Operation Result/ Explanation
+1. Remove non-alphanumeric (isalnum)
+characters
+"AmanaplanacanalPanama"
+2. Convert all to lowercase "amanaplanacanalpanama" (tolower)
+3. Compare characters Compare from both ends → all
+characters match
+4. Final Check Reads same forward & backward
+ Output:
+true
+*/
