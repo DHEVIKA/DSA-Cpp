2a089dcc79da3987b18740a6b78553130b0ba9f0
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-05-11 23:00:17 +0530

    Time: 8 ms (28.48%), Space: 16.5 MB (7.95%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -1,19 +1,28 @@
+
 class Solution {
 public:
     vector<int> separateDigits(vector<int>& nums) {
-        
-        vector<int> result;
+        vector<int> res;
 
         for (int num : nums) {
-
-            string s = to_string(num);
-
-            for (char ch : s) {
-
-                result.push_back(ch - '0');
-            }
+            vector<int> digits = getDigits(num);
+            res.insert(res.end(), digits.begin(), digits.end());
         }
 
-        return result;
+        return res;
+    }
+
+private:
+    vector<int> getDigits(int num) {
+        vector<int> digits;
+
+        while (num > 0) {  // num = 13
+            digits.push_back(num % 10); // to extract the last digit 13%10 -> 3
+            num /= 10; //  to extract the 1st digit 13/10 -> 1
+        }
+
+        reverse(digits.begin(), digits.end()); //3 1 came need to change as 13 so do reverse
+
+        return digits;
     }
 };
