601c101a360ddf9694b4d3aa62543aad04ada4b1
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-05-12 15:41:46 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.88%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,61 +1,53 @@
 class Solution {
 public:
-    void merge(vector<int>& a, int m, vector<int>& b , int n) {
-     int index = m+n-1 ;	 // Position to insert at in nums1
-   int i = m - 1;      	 // Last index of actual elements in nums1
-   int j = n - 1;       	 // Last index of nums2
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int  i = m-1;
+        int j = n-1;
+        int k = m+n-1; // position from last 
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
 
-
-   // Merge both arrays starting from the back
-   while (i >= 0 && j >= 0) {
-       if (a[i] >= b[j]) {
-           a[index--] = a[i--];
-       } else {
-           a[index--] = b[j--];
-       }
-   }
-
-
-   // If any elements left in nums2, copy them over
-   while (j >= 0) {
-       a[index--] = b[j--];
-   }
-
-        
+        }
+        while(j>=0){
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
     }
 };
 
-// TC -> O(M+N)
-// SC -> O(1)
-/* noo need use extra space theres a space in arr1 so we can reuse it so we r tracking from last infex m+n-1 __ , __ , __
-nums1 = [1,2,3,_,_,_]
-nums2 = [2,5,6]
 
-nums1: [1, 2, 3, _, _, _]
-              ↑        ↑
-             p1      curr
+/*  Intuition:
+Think of nums1 and nums2 as two sorted queues waiting to be merged.
+Instead of starting from the front (which could overwrite data), we start from the back — the largest elements
+first — and place them at the end of nums1.
+This way, every position is safely filled without disturbing the remaining unmerged elements.
 
-nums2: [2, 5, 6]
-              ↑
-             p2
+Quote
+“Sometimes stepping back (or iterating backward) is the smartest way forward.” 
 
-“I always place the largest element at curr and move backwards.”
- nums1[p1]=3 vs nums2[p2]=6
-6 is bigger place it
- nums1: [1, 2, 3, _, _, 6]
-                       ↑
-                     curr
-  p2--, curr--
- 3 vs 5
- 5 is bigger place it
-  nums1: [1, 2, 3, _, 5, 6]
-                    ↑
-                  curr      
-3 vs 2
-3 is bigger place it
-nums1: [1, 2, 3, 3, 5, 6]
-                 ↑
-               curr
+nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Explanation:
+Step i j k nums1(after
+operation)
+Action
+1. 2 2 5 [1,2,3,0,0,6] 6 from nums2
+placed
+2. 2 1 4 [1,2,3,0,5,6] 5 from nums2
+placed
+3. 2 0 3 [1,2,3,3,5,6] 3 from nums1
+placed
+4. 1 0 2 [1,2,2,3,5,6] 2 from nums2
+placed
+5. 0 -1 1 [1,2,2,3,5,6] Done (nums2
+exhausted
 
-2 vs 2
-take nums2 (or either) */
+*/
