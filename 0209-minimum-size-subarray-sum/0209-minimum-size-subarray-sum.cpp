7ede4573afecacf63b695d4379a4d910dcc94105
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-05-14 12:29:29 +0530

    Time: 0 ms (100%), Space: 32.2 MB (5.89%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int left=0;
-        int right=0;
-        int sum=0;
-        int minlen=INT_MAX;
-        while(right<nums.size()){
-            sum+=nums[right];
-            while(sum>=target){
-                sum-=nums[left];
-                minlen=min(right-left+1,minlen);
+        int minLen = INT_MAX;
+        int left = 0;
+        int curSum = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            curSum += nums[right]; // EXPAND RIGHT
+
+            while (curSum >= target) {
+                if (right - left + 1 < minLen) {  // SHRINK LEFT
+                    minLen = right - left + 1; 
+                }
+                curSum -= nums[left]; // REMOVE LEFT MOST
                 left++;
             }
-            right++;
         }
-        if(minlen==INT_MAX){
-            return 0;
-        }
-        return minlen;
+
+        return minLen != INT_MAX  ? minLen : 0;        
     }
 };
+
+/* use 2 pointers to maintain the sliding window either left or right or keep left n track right
+Expand the window (right++) to increase the sum until it becomes >= target
+once the sum is larger enough:
+    shrink the window from the left(left++) to making it smaller
+    Track the minimum size of window
+Repeat until right reaches to end */
