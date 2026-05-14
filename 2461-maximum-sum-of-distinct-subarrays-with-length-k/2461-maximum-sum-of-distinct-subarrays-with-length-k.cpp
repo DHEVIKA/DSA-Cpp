2910ffaeea26a968b2182a8dac2b112531b740b2
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-05-14 12:12:09 +0530

    Time: 99 ms (71.75%), Space: 95.4 MB (55.39%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,54 +1,33 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> freq; // to calculate duplicates
-        long long windowSum = 0, maxSum = 0;
-        int l = 0;
-
-        for (int r = 0; r < nums.size(); r++) {
-            windowSum += nums[r];
-            freq[nums[r]]++;
-
-            if (r - l + 1 > k) {  //if the length is exceeded k 
-                windowSum -= nums[l];
-                freq[nums[l]]--;
-                if (freq[nums[l]] == 0) {
-                    freq.erase(nums[l]);
+        int n = nums.size();
+        unordered_set<int> elements;
+        long long current_sum = 0;
+        long long max_sum = 0;
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            if (elements.find(nums[right]) == elements.end()) {   // to avoid duplicate  we use set checks if the current element (nums[right]) is NOT present in the set elements
+                current_sum += nums[right]; // add the right element 
+                elements.insert(nums[right]); // put inside the set
+                
+                if (right -left + 1 == k) { //codition length == k
+                    max_sum = max(max_sum, current_sum); //update the max
+                    current_sum -= nums[left ]; // remove the left most 
+                    elements.erase(nums[left ]); // remove from set also
+                    left ++;
                 }
-                l++;
-            }
-
-            if (r - l + 1 == k && freq.size() == k) {
-                maxSum = max(maxSum, windowSum);
+            } else {
+                while (nums[left ] != nums[right]) {
+                    current_sum -= nums[left ];
+                    elements.erase(nums[left ]);
+                    left ++;
+                }
+                left ++;
             }
         }
-
-        return maxSum;
+        
+        return max_sum;
     }
 };
-
-/* Here, we need to find a subarray of length k such that:
--> All elements are distinct
--> The sum is maximized
-
-A brute-force approach would check every subarray of size k and verify if all elements are distinct, but that would take O(n · k) time, which is too slow.
-
-Instead, we observe that:
--> We are only dealing with fixed-size windows (k)
--> So we can use a sliding window to efficiently move from one subarray to the next
-
-As we slide the window:
--> We add the new element entering the window
--> We remove the element leaving the window
--> We maintain a frequency map to track occurrences of elements in the current window
--> We also maintain the current sum of the window
-
-To check if all elements are distinct:
-If the number of unique elements(length of frequency map) in the map equals k, then all elements are distinct
-Otherwise, duplicates exist
-
-This avoids rechecking the entire window every time and allows us to update everything in constant time per step.
-
-Thus, we efficiently compute the answer in O(n) time using a sliding window and hash map.
-
-*/
