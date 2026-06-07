193edcfb8d6460e7463f6ea2d67a01a1f0bcde4a
Dhevika M <104631091+DHEVIKA@users.noreply.github.com>
2026-06-07 13:17:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (68.44%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        string res = ""; // Acts like a stack
-        for (char c : s) {
-            if (isdigit(c)) {
-                if (!res.empty()) {
-                    res.pop_back(); // Remove the last character
-                }
-            } else {
-                res.push_back(c); // Add character to stack
-            }
+        int i=0, j=0;
+        string ans = "";
+        while(i < s.size()){
+            if(!isdigit(s[i])) ans+=s[i];
+            else ans.pop_back();
+            i++;
         }
-        return res;
+        return ans;
     }
 };
-
-/* Intuition
-Think of a stack like a pile of plates: Last In, First Out (LIFO) 🍽️.
-We process characters in a string like a clean-up operation 🧹, removing digits based on their position.
-
-Stack helps when order matters, and we might need to undo things ⏪.
-If a digit follows a letter, we remove both!
-*/
